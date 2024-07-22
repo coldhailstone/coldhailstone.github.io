2a65fe088d122daa72c8489f66aac0a87aac833d
coldhailstone <cksn1993@gmail.com>
2024-07-22 21:16:36 +0900

    포스트 추가

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-function solution(n) {
-    let answer = 0;
-    for (let i = 1; i <= n; i++) {
-        answer += i;
+function solution(arr) {
+    // let answer = Math.min(...arr);
+    let answer,
+        min = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
     }
+    answer = min;
     return answer;
 }
 
-console.log(solution(6));
-console.log(solution(10));
+let arr = [5, 3, 7, 11, 2, 15, 17];
+console.log(solution(arr));
